77c5c4bc818e0d454557349874f1932a75a88f55
mforstchen <71297558+mforstchen@users.noreply.github.com>
2020-10-29 17:08:20 -0400

    Update Meghan Forstchen Homework 9.R

--- Meghan Forstchen Homework 9.R
+++ Meghan Forstchen Homework 9.R
@@ -1,46 +1,50 @@
+
+(((This ended up being spread over several functions because I couldn't get it to run in one and it is not fully functional :(  )))
 #Homework 9 Assignment
 
 #Make a custom function that reads the contents of a directory
 #Function should: use "dir" argument, find coefficient of variation from
-#optional column, 
+#optional column
 
+#Set my working directory
 setwd("~/Desktop/TestFile")
+
 
 #Get a list of all files in the directory that you will be applying function to
 #I am working with a practice directory called "TestFile" on my Desktop
 
-listcsv <- dir(pattern = "*.csv")
+dir=dir(path="~/Desktop/TestFile", pattern=".csv")
 
-#Create a location to hold outputs
-OutputList <- list()
+#Create a user specified column for the data
+Column = 1
 
-for (i in 1:length(listcsv)){
-  OutputList[[i]] <- read.csv(listcsv[i])
-}
-str(OutputList[[1]]) 
 
-TestFilesFunction <- function(x){
+#Read in the files from the directory
+  for (i in 1:length(dir)){
+    dt <- paste("dt", i, sep = "_")
+    assign(dt, read.csv(dir[i], header=TRUE))
+  }
+
+#Coefficient of VarianceFunction
+CoefficientVarianceFunction <- function(x){
+     
   MeanFiles <- mean(x)
   StandardDeviationFiles <- sd(x)
   CoefficientVarianceFiles <- StandardDeviationFiles/MeanFiles
   return(CoefficientVarianceFiles)
-}
+  }
 
-FirstTrialOutput <- lapply(Book3, TestFilesFunction)
-
-
+#Warning message function if there are less than 50 observations
 WarningMessage <- function(x){
   if(nrow(x) < 50) {warning("There are less than 50 observations")}
   return()
 }
-
+#Error Messge if there are less than 50 observation
 ErrorMessage <- function(x){
   if(nrow(x) < 50) {stop("There are less than 50 observations")}
   return()
 }
 
-WarningMessage(Book3)
-ErrorMessage(Book3)
 
 
 
